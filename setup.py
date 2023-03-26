58749e8117b532f5ee9bca9c79ff54aad8a8d41e
NikhilRN18 <107941991+NikhilRN18@users.noreply.github.com>
2023-03-26 00:43:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Ioannis Prapas, Spyros Kondylatos",
     author_email="",
     url="https://github.com/Orion-AI-Lab/daily-wildfire-danger",
-    install_requires=["pytorch-lightning>=1.2.0", "hydra-core>=1.0.6", "torch==1.12.0", "torchvision==0.12.0",
+    install_requires=["pytorch-lightning>=1.2.0", "hydra-core>=1.0.6", "torch==1.11.0", "torchvision==0.12.0",
                       "pytorch-lightning==1.5.8", "fastai==2.7.8"],
     packages=find_packages(),
 )
